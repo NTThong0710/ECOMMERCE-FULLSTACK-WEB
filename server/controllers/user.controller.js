be85139965b0ab8822be9f0306f28d53147a4da5
NTThong0710 <thongtran7102004@gmail.com>
2025-11-27 07:42:54 +0700

    feat: Upload, delete image and forgot password API

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,7 +6,7 @@ import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 import generatedAccessToken from "../utils/generatedAccessToken.js";
 import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 import {v2 as cloudinary} from 'cloudinary'
-import fs from 'fs'
+import fs, { access } from 'fs'
 import { error } from "console";
 
 cloudinary.config({ 
@@ -374,6 +374,233 @@ export async function updateUserDetails(request, response) {
 
 
     } catch ( error){
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//forgot password
+export async function forgotPasswordController(request, response){
+    try {
+        const {email} = request.body
+
+        const user = await UserModel.findOne({email:email})
+
+        if(!user){
+            return response.status(400).json({
+                message : "Email không phù hợp",
+                error : true,
+                success: false
+            })
+        }
+
+        else {
+            let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+            user.otp = verifyCode
+            user.otpExpires = Date.now() + 600000
+
+            await user.save()
+
+            await sendEmailFun({
+                to: email,
+                subject: "Xác thực email từ Ecommerce App",
+                text: "",
+                html: verifyEmailTemplate(user.name, verifyCode)
+            })
+
+            return response.json({
+                message : "Kiểm tra email của bạn",
+                error : false,
+                success : true
+            })
+        }
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//verify forgot password
+export async function verifyForgotPasswordOtp(request, response) {
+    try{
+        const {email, otp}= request.body;
+        const user = await UserModel.findOne({email:email})
+        
+        if(!user){
+            return response.status(400).json({
+                message : "Email không phù hợp",
+                error : true,
+                success: false
+            })
+        }
+
+        if(!email || !otp){
+            return response.status(400).json({
+                message : "Vui lòng cung cấp email, otp",
+                error: true,
+                success: false
+            })
+        }
+
+        if(otp !== user.otp){
+            return response.status(400).json({
+                message : "OTP không phù hợp",
+                error: true,
+                success: false
+            })
+        }
+
+        const currentTime = new Date().toISOString()
+
+        if (user.otpExpires < currentTime) {
+            return response.status(400).json({
+                message: "OTP đã quá hạn",
+                error: true,
+                success: false
+            })
+        }
+
+        user.otp = "";
+        user.otpExpires = "";
+
+        await user.save()
+
+        return response.status(400).json({
+            message : "Xác thực OTP thành công",
+            error: true,
+            success: false
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//reset password
+export async function resetPassword(request, response) {
+    try{
+        const {email, newPassword, confirmPassword} = request.body
+        if(!email || !newPassword || !confirmPassword){
+            return response.status(400).json({
+                message : "Vui lòng cung cấp đầy đủ email, newPassword, confirmPassword"
+            })
+        }
+        const user = await UserModel.findOne({email});
+        if(!user){
+            return response.status(400).json({
+                message : "Email không hợp lệ",
+                error : true,
+                success : false
+            })
+        }
+
+        if( newPassword !== confirmPassword){
+            return response.status(400).json({
+                message : "Thông tin về mật khẩu cần trùng khớp với nhau",
+                error : true,
+                success : false
+            })
+        }
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashPassword = await bcryptjs.hash(confirmPassword,salt)
+        
+        
+        user.password= hashPassword
+        await user.save()
+
+        return response.json({
+            message : "Cập nhật mật khẩu thành công",
+            error : false,
+            success : true
+        })
+
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//refresh token controller
+export async function refreshToken(request, response) {
+    try {
+        const refreshToken = request.cookies.refreshToken || request?.header?.authorization?.split(" ")[1] //Bearer token
+        if (!refreshToken){
+            return response.status(400).json({
+                message : "Token không hợp lệ",
+                error : true,
+                success : false
+            })
+        }
+
+        const verifyToken = await jwt.verify(refreshToken, process.env.SECRET_KEY_REFRESH_TOKEN)
+        if (!verifyToken){
+             return response.status(401).json({
+                message : "Token đã quá hạn",
+                error : true,
+                success : false
+            })
+        }
+        const userId =verifyToken?._id
+        const newAccessToken = await generatedAccessToken(userId)
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+
+        response.cookie('accessToken',newAccessToken,cookiesOption)
+
+        return response.json({
+            message : "AccessToken mới đã được tạo",
+            error: false,
+            success: true,
+            data : {
+                accessToken : newAccessToken
+            }
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+// get login user details
+export async function userDetails(request, response) {
+    try {
+        const userId = request.userId
+
+        const user = await UserModel.findById(userId).select('-password -refresh_token')
+
+        return response.json({
+            message : 'Chi tiết users',
+            data : user,
+            error : false,
+            success : true
+
+        })
+
+    } catch (error){
         return response.status(500).json({
             message : error.message || error,
             error : true,
